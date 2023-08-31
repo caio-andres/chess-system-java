4bd68f0f07ec6a045a25e574e3f1e54b60ab81c5
Caio Andre <caioandresgit@outlook.com>
2023-08-31 19:05:39 -0300

    Possible moves of a piece system

---
+++ src/boardgame/PieceException.java
@@ -0,0 +1,10 @@
+package boardgame;
+
+public class PieceException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public PieceException(String msg) {
+		super(msg);
+	}
+
+}
